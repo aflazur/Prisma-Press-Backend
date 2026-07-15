3a222d2547f27a3ed8fefa3e56264ce31f083476
aflazur <mdaflazurrahaman2022@gmail.com>
2026-07-15 23:23:47 +0600

    Defining Catch Async As Higher Order Function

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const registerUser = async (req: Request, res: Response) => {
-   try {
-     const payload = req.body;
+const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
 
     const user = await userService.registerUserIntoDB(payload);
 
@@ -14,17 +14,7 @@ const registerUser = async (req: Request, res: Response) => {
         message: "User registered successfully",
         data: { user }
     })
-   } catch (error) {
-    console.error("Error registering user:", error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-        message: "Error registering user",
-        error: error instanceof Error ? error.message : "Unknown error",
-    });
-    
-   }
-}
+})
 
 export const userController = {
     registerUser,
