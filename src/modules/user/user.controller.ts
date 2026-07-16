22a281f638517528d23efa98858429acb37f397e
aflazur <mdaflazurrahaman2022@gmail.com>
2026-07-16 23:02:55 +0600

    Implementing Login User and jwt for access token and refresh token

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,13 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+
+
 
 const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
     const user = await userService.registerUserIntoDB(payload);
 
-    res.status(httpStatus.CREATED).json({
+    // res.status(httpStatus.CREATED).json({
+    //     success: true,
+    //     statusCode: httpStatus.CREATED,
+    //     message: "User registered successfully",
+    //     data: { user }
+    // })
+
+    sendResponse(res, {
         success: true,
         statusCode: httpStatus.CREATED,
         message: "User registered successfully",
