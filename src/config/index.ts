c52e258beedc41e1a4a295746241513bbed6d07a
aflazur <mdaflazurrahaman2022@gmail.com>
2026-07-22 23:16:16 +0600

    Now complete my project

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,8 @@ import dotenv from "dotenv";
 import path from "path";
 
 
-dotenv.config({path: path.join(process.cwd(), ".env")});
+dotenv.config({path: path.join(process.cwd(), ".env") });
+
 
 export default {
     port : process.env.PORT,
@@ -13,4 +14,7 @@ export default {
     jwt_refresh_secret : process.env.JWT_REFRESH_SECRET!,
     jwt_access_expires_in : process.env.JWT_ACCESS_EXPIRES_IN!,
     jwt_refresh_expires_in : process.env.JWT_REFRESH_EXPIRES_IN!,
+    stripe_product_price_id : process.env.STRIPE_PRODUCT_PRICE_ID!,
+    stripe_secret_key : process.env.STRIPE_SECRET_KEY!,
+    stripe_webhook_secret : process.env.STRIPE_WEBHOOK_SECRET!
 }
